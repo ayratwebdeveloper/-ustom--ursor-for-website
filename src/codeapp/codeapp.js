bdb4c49597544761c628bb06dfff8c40a55241ab
ayratdeveloper <ayratdeveloper@gmail.com>
2023-06-05 10:10:32 +0400

    added codeapp.js

---
+++ src/codeapp/codeapp.js
@@ -0,0 +1,24 @@
+const cursor = document.querySelector('.custom_cursor');
+
+// Следим за движением мыши
+window.addEventListener('mousemove', (e) => {
+		cursor.style.left = e.pageX + 'px';
+		cursor.style.top  = e.pageY + 'px';
+		cursor.setAttribute('data-fromTop', (cursor.offsetTop - scrollY));
+});
+
+window.addEventListener('scroll', () => {
+		const fromTop    = cursor.getAttribute('data-fromTop');
+		cursor.style.top = scrollY + parseInt(fromTop) + 'px';
+		console.log(scrollY);
+});
+
+window.addEventListener('click', () => {
+	if (cursor.classList.contains('click')) {
+		cursor.classList.remove("click");
+		void cursor.offsetWidth; // Обновляем DOM
+		cursor.classList.add("click");
+	} else {
+		cursor.classList.add("click");
+	}
+});
